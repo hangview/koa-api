5ddda1fdd8a3f03efa08bd96d1452229ffcf82e8
魏杭 <hang.wei@corp.elong.com>
2018-08-29 18:21:15 +0800

    add api

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-const router = require('koa-router')();
+const Router = require('koa-router');
 const musicList = require('../mock/music');
+let router = new Router();
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -15,6 +16,14 @@ router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   }
+})
+
+router = new Router({
+  prefix: '/api/list'
+})
+
+router.get('/:id', async (ctx, next) => {
+  ctx.body = musicList[ctx.params.id];
 })
 
 module.exports = router
