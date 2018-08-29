4f3d826b62a8e9d56ae4fe65e7264ccc969b743d
魏杭 <hang.wei@corp.elong.com>
2018-08-29 18:27:41 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,15 @@ const Router = require('koa-router');
 const musicList = require('../mock/music');
 let router = new Router();
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+router = new Router({
+  prefix: '/api'
 })
 
-router.get('/api/fetchList', async (ctx, next) => {
+router.get('/fetchList', async (ctx, next) => {
   ctx.body = musicList;
 })
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
-
-router = new Router({
-  prefix: '/api/list'
-})
-
-router.get('/:id', async (ctx, next) => {
+router.get('/list/:id', async (ctx, next) => {
   ctx.body = musicList[ctx.params.id];
 })
 
